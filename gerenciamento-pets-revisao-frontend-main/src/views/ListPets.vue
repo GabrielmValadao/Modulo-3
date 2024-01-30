6d037dbe0d8394e5b971de5b3ae9a6053dd77e4f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-01-30 20:06:23 -0300

    feat: adiciona mais tabelas e dados a tabela de pets

--- gerenciamento-pets-revisao-frontend-main/src/views/ListPets.vue
+++ gerenciamento-pets-revisao-frontend-main/src/views/ListPets.vue
@@ -4,12 +4,18 @@
       <tr>
         <th class="text-left">Nome</th>
         <th class="text-left">Raça</th>
+        <th class="text-left">Porte</th>
+        <th class="text-left">Idade</th>
+        <th class="text-left">Peso</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="pet in pets" :key="pet.id">
         <td>{{ pet.pet_name }}</td>
         <td>{{ pet.race.name }}</td>
+        <td>{{ pet.size }}</td>
+        <td>{{ pet.age }}</td>
+        <td>{{ pet.weight }}</td>
       </tr>
     </tbody>
   </v-table>
