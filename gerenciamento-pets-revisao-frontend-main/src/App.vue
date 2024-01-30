1681747630755d60e6ceff1cc9096125a364cf00
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-01-30 20:00:52 -0300

    feat: criação e implementação do componente Menu.vue

--- gerenciamento-pets-revisao-frontend-main/src/App.vue
+++ gerenciamento-pets-revisao-frontend-main/src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <v-layout>
-    <v-navigation-drawer permanent>
-      <v-list>
-        <v-list-item
-          prepend-avatar="https://avatars.githubusercontent.com/u/135287636?s=400&u=5d00338a434031175b33d5dd9b753d8637d1050c&v=4"
-          title="Gabriel Valadão"
-          subtitle="metzdorfgabriel@gmail.com"
-        ></v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-folder" title="Pets" value="myfiles"></v-list-item>
-        <v-list-item prepend-icon="mdi-star" title="Sair" value="starred"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
+    <Menu />
     <v-main>
       <router-view></router-view>
     </v-main>
   </v-layout>
 </template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
---
+++ gerenciamento-pets-revisao-frontend-main/src/components/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout>
+    <v-navigation-drawer permanent>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://avatars.githubusercontent.com/u/135287636?s=400&u=5d00338a434031175b33d5dd9b753d8637d1050c&v=4"
+          title="Gabriel Valadão"
+          subtitle="metzdorfgabriel@gmail.com"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="Pets" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Sair" value="starred"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
+</template>
