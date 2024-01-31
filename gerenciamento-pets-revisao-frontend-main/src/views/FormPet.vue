24382eb6626ff90a1ac2baa3bd52fec24ad057fd
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-01-30 22:05:51 -0300

    feat: desenvolve formulario de cadastro de pet

--- gerenciamento-pets-revisao-frontend-main/src/views/FormPet.vue
+++ gerenciamento-pets-revisao-frontend-main/src/views/FormPet.vue
@@ -1,1 +1,84 @@
-<template></template>
+<template>
+  <v-card width="80%" class="mx-auto px-6 pt-8" title="Cadastro de pet">
+    <v-row>
+      <v-col cols="12" md="8">
+        <v-text-field label="Nome" variant="outlined" v-model="name" />
+      </v-col>
+      <v-col cols="12" md="2" sm="6">
+        <v-text-field label="Idade" variant="outlined" v-model="age" />
+      </v-col>
+      <v-col cols="12" md="2" sm="6">
+        <v-text-field label="Peso" variant="outlined" v-model="weight" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-select
+          label="Porte"
+          :items="itemsSize"
+          variant="outlined"
+          placeholder="Selecione o porte do seu pet"
+          v-model="size"
+        />
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <v-select
+          label="Espécie"
+          :items="itemsSpecies"
+          variant="outlined"
+          placeholder="Selecione uma espécie"
+          v-model="specie"
+          item-title="name"
+          item-value="id"
+        />
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-select
+          label="Raça"
+          :items="itemsRaces"
+          variant="outlined"
+          placeholder="Selecione uma raça"
+          v-model="race"
+          item-title="name"
+          item-value="id"
+        />
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+import { optionsSize } from '../constants/pets.constans'
+import SpecieService from '../services/SpecieService'
+import RaceService from '../services/RaceService'
+
+export default {
+  data() {
+    return {
+      name: '',
+      age: 1,
+      weight: 1,
+      size: '',
+      specie: '',
+      race: '',
+
+      itemsSize: optionsSize,
+      itemsSpecies: [],
+      itemsRaces: []
+    }
+  },
+  mounted() {
+    SpecieService.getAllSpecies()
+      .then((data) => {
+        this.itemsSpecies = data
+      })
+      .catch(() => alert('Houve um erro ao buscar as opções'))
+    RaceService.getAllRaces().then((data) => {
+      this.itemsRaces = data
+    })
+  }
+}
+</script>
+
+<style scoped></style>
