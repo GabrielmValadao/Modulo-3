b6d91ad7b9b9428530d1ff77427cc4d7e8a65d3a
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-01-31 19:21:58 -0300

    feat: cria botão no formulário

--- gerenciamento-pets-revisao-frontend-main/src/views/FormPet.vue
+++ gerenciamento-pets-revisao-frontend-main/src/views/FormPet.vue
@@ -1,51 +1,57 @@
 <template>
-  <v-card width="80%" class="mx-auto px-6 pt-8" title="Cadastro de pet">
-    <v-row>
-      <v-col cols="12" md="8">
-        <v-text-field label="Nome" variant="outlined" v-model="name" />
-      </v-col>
-      <v-col cols="12" md="2" sm="6">
-        <v-text-field label="Idade" variant="outlined" v-model="age" />
-      </v-col>
-      <v-col cols="12" md="2" sm="6">
-        <v-text-field label="Peso" variant="outlined" v-model="weight" />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12" md="4">
-        <v-select
-          label="Porte"
-          :items="itemsSize"
-          variant="outlined"
-          placeholder="Selecione o porte do seu pet"
-          v-model="size"
-        />
-      </v-col>
+  <form @submit.prevent="handleSubmit">
+    <v-card width="80%" class="mx-auto px-6 pt-8" title="Cadastro de pet">
+      <v-row>
+        <v-col cols="12" md="8">
+          <v-text-field label="Nome" variant="outlined" v-model="name" />
+        </v-col>
+        <v-col cols="12" md="2" sm="6">
+          <v-text-field label="Idade" variant="outlined" v-model="age" />
+        </v-col>
+        <v-col cols="12" md="2" sm="6">
+          <v-text-field label="Peso" variant="outlined" v-model="weight" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-select
+            label="Porte"
+            :items="itemsSize"
+            variant="outlined"
+            placeholder="Selecione o porte do seu pet"
+            v-model="size"
+          />
+        </v-col>
 
-      <v-col cols="12" md="4">
-        <v-select
-          label="Espécie"
-          :items="itemsSpecies"
-          variant="outlined"
-          placeholder="Selecione uma espécie"
-          v-model="specie"
-          item-title="name"
-          item-value="id"
-        />
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-select
-          label="Raça"
-          :items="itemsRaces"
-          variant="outlined"
-          placeholder="Selecione uma raça"
-          v-model="race"
-          item-title="name"
-          item-value="id"
-        />
-      </v-col>
-    </v-row>
-  </v-card>
+        <v-col cols="12" md="4">
+          <v-select
+            label="Espécie"
+            :items="itemsSpecies"
+            variant="outlined"
+            placeholder="Selecione uma espécie"
+            v-model="specie"
+            item-title="name"
+            item-value="id"
+          />
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-select
+            label="Raça"
+            :items="itemsRaces"
+            variant="outlined"
+            placeholder="Selecione uma raça"
+            v-model="race"
+            item-title="name"
+            item-value="id"
+          />
+        </v-col>
+      </v-row>
+
+      <v-card-actions class="d-flex justify-end">
+        <v-btn color="orange" type="submit" variant="flat"> Cadastrar </v-btn>
+      </v-card-actions>
+    </v-card>
+  </form>
 </template>
 
 <script>
@@ -68,6 +74,7 @@ export default {
       itemsRaces: []
     }
   },
+
   mounted() {
     SpecieService.getAllSpecies()
       .then((data) => {
@@ -77,6 +84,10 @@ export default {
     RaceService.getAllRaces().then((data) => {
       this.itemsRaces = data
     })
+  },
+
+  methods: {
+    handleSubmit() {}
   }
 }
 </script>
