b65028e2a27d2e30a48b4b7e7a7d042124e83fa8
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-01-31 22:29:30 -0300

    feat: cria autenticação de login

---
+++ gerenciamento-pets-revisao-frontend-main/src/services/AuthenticationService.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+class AuthenticationService {
+  async login(body) {
+    const response = await axios.post('http://127.0.0.1:8000/api/login', body)
+    return response.data
+  }
+}
+
+export default new AuthenticationService()
