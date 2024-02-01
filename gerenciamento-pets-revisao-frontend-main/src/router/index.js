705c337cd74d3983c60959eda7ab71770eb5bdcb
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-01-31 22:14:51 -0300

    feat: importa a pagina login no router

--- gerenciamento-pets-revisao-frontend-main/src/router/index.js
+++ gerenciamento-pets-revisao-frontend-main/src/router/index.js
@@ -3,11 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ListPets from '../views/ListPets.vue'
 import FormPet from '../views/FormPet.vue'
+import Login from '../views/Login.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
     },
