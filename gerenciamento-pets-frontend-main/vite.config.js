d2f19c1ba55c94ed321e0bad5f5453a5b7f72b3c
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-02-06 19:34:37 -0300

    feat: ajusta arquivo de vite.config para aceitar vuetify

--- gerenciamento-pets-frontend-main/vite.config.js
+++ gerenciamento-pets-frontend-main/vite.config.js
@@ -5,9 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    server: {
+      deps: {
+        inline: ['vuetify']
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
